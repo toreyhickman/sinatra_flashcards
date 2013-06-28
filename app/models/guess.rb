610a8c5bbc52beec3e47aadbd5b1826343237159
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-28 14:10:13 -0500

    model bug

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,6 +2,7 @@ class Guess < ActiveRecord::Base
   validates :card_id, :presence => true
   validates :round_id, :presence => true
   validates :correct, :presence => true
+  validates :response, :presence => true
 
   belongs_to :card
   belongs_to :round
