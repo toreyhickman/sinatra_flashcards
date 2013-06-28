2afed461b9e96e69f7ac0fde18e40255ce850f33
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-28 16:23:15 -0500

    updating css

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ end
 
 5.times { fake_user }
 
-addition_deck = Deck.new(:name => "Simple Addition")
+addition_deck = Deck.new(:name => "Simple Addition", :description => "Practice your addition math facts.")
 addition_deck.save
 
 Card.create(:prompt => "5 + 6", :answer => "11", :deck_id => 1)
@@ -13,7 +13,7 @@ Card.create(:prompt => "7 + 9", :answer => "16", :deck_id => 1)
 Card.create(:prompt => "3 + 4", :answer => "7", :deck_id => 1)
 Card.create(:prompt => "4 + 8", :answer => "12", :deck_id => 1)
 
-subtraction_deck = Deck.new(:name => "Simple Substitution")
+subtraction_deck = Deck.new(:name => "Simple Substitution", :description => "Practice your subtraction math facts.")
 subtraction_deck.save
 
 Card.create(:prompt => "9 - 4", :answer => "5", :deck_id => 2)
